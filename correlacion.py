f1c5ad2f8bfacd822e120d3dd8636a059b790093
Lorena <lorena@lorena.dieste>
2020-07-08 15:29:23 +0200

    Correlation chnages

--- correlacion.py
+++ correlacion.py
@@ -36,9 +36,15 @@ minMass = min(mass)
 signal_d = pd.DataFrame(signal_d)
 background_d = pd.DataFrame(background_d)
 aux_s = dict()
+aux_s_e = dict()
+for i in range(int(minMass),int(maxMass) - 20,20):
+	aux_s['range'+str(i)+'-'+str(i+20)] = signal_d[(signal_d['Ds_M']>i) & (signal_d['Ds_M']<i+20)]
+mass = set_aux['X_test']['etap_M']
+maxMass = max(mass)
+minMass = min(mass)
 for i in range(int(minMass),int(maxMass) - 10,10):
-	aux_s['range'+str(i)+'-'+str(i+10)] = signal_d[(signal_d['Ds_M']>i) & (signal_d['Ds_M']<i+10)]			
-fig, axs = plt.subplots(7, 7, figsize=(15, 6), constrained_layout=True, facecolor='w', edgecolor='k')
+	aux_s_e['range'+str(i)+'-'+str(i+10)] = signal_d[(signal_d['etap_M']>i) & (signal_d['etap_M']<i+10)]			
+fig, axs = plt.subplots(4, 6, figsize=(15, 6), constrained_layout=True, facecolor='w', edgecolor='k')
 i = 0
 
 axs = axs.ravel()
@@ -48,11 +54,25 @@ for m in aux_s:
 		continue
 	for k in signal_d.keys():
 		if k != 'Ds_M' and k != 'etap_M':			
-			axs[i].hist(aux_s[m][k],histtype='step',label=k)
+			axs[i].hist(aux_s[m][k],histtype='step',label=k+" Ds_M")
+			axs[i].set_title(m)
+			handles, labels = axs[i].get_legend_handles_labels()			
+	i+=1
+fig.legend(handles, labels, loc='lower right')
+fig.savefig("Ds_M.png")		
+plt.show()			
+fig, axs = plt.subplots(4, 5, figsize=(15, 6), constrained_layout=True, facecolor='w', edgecolor='k')
+i = 0
+axs = axs.ravel()
+for m in aux_s_e:
+	if not bool(m) :
+		continue
+	for k in signal_d.keys():
+		if k != 'Ds_M' and k != 'etap_M':			
+			axs[i].hist(aux_s_e[m][k],histtype='step',label=k+" etap_M")
 			axs[i].set_title(m)
 			handles, labels = axs[i].get_legend_handles_labels()			
 	i+=1	
-
 fig.legend(handles, labels, loc='lower right')
-fig.savefig("intento2.png")		
+fig.savefig("etap_M.png")		
 plt.show()		
